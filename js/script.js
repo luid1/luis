acfb3c6447e8e002e74e0a787dca1ca216058dfb
luid <79557296+luid1@users.noreply.github.com>
2025-09-04 14:11:19 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+let navbar = document.querySelector('.header .navbar');
+let menuBtn = document.querySelector('#menu-btn');
+let closeBtn = document.querySelector('#close-navbar');
+
+menuBtn.onclick = () =>{
+   navbar.classList.add('active');
+};
+
+closeBtn.onclick = () =>{
+    navbar.classList.remove('active');
+ };
+
+window.onscroll = () =>{
+   navbar.classList.remove('active');
+};
+
+var loader = document.getElementById('preloader');
+
+window.addEventListener("load", function(){
+    loader.style.display = "none";
+})
